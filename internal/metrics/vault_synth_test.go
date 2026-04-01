f17490510cb7db97b2492a022b28dc88a8c850bc
Christopher Nolan <christopher.nolan@intel.com>
2026-04-01 16:42:56 +0000

    linter fixes

--- internal/metrics/vault_synth_test.go
+++ internal/metrics/vault_synth_test.go
@@ -1,7 +1,7 @@
-// SPDX-FileCopyrightText: (C) 2025 Intel Corporation
+// SPDX-FileCopyrightText: (C) 2026 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 
-package metrics
+package metrics //nolint:revive,nolintlint // Package name metrics is intentional
 
 import (
 	"bytes"
